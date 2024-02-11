cc3485752efb0b3dbd53c77a155590032fc2527b
jesuskcapellan <122330858+jesuskcapellan@users.noreply.github.com>
2024-02-11 15:41:03 -0500

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -4,6 +4,7 @@ echo "Generating a new SSH key for GitHub..."
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+mkdir ~/.ssh
 ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
