eb9c1805616ed738fa2905a9ec67807f109f797d
jesuskcapellan <122330858+jesuskcapellan@users.noreply.github.com>
2024-02-11 18:02:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,29 @@
 
 echo "Setting up your Linux environment..."
 
-echo "Installing Git..."
-
 # Install Git
 # https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+echo "Installing Git..."
 sudo apt-get update
 sudo apt-get install git
 
-# Generate SSH key
+# Install Stow
+echo "Installing Stow..."
+sudo apt-get install stow
 
-# 
+# Install Neovim
+mkdir ~/.git-clones
+sudo apt-get install ninja-build gettext cmake unzip curl
+git clone https://github.com/neovim/neovim ~/.git-clones/nvim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git checkout stable
+sudo make install
+cd
+
+# Install tmux
+sudo apt-get install tmux
+
+# Clone dotfiles repo
+git clone https://github.com/jesuskcapellan/dotfiles.git ~/dotfiles
+cd dotfiles
+stow .
