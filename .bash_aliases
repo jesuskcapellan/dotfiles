6b23d45f17bddb046d529a85c985f64949299a7d
Jesus Capellan <jcapellan816@gmail.com>
2024-02-11 21:25:20 -0500

    Modify tmux and themes

--- .bash_aliases
+++ .bash_aliases
@@ -1,4 +1,6 @@
 alias confetti="nvim ~/.config/nvim"
 alias rebash="source ~/.bashrc"
 alias bashit="nvim ~/.bashrc"
+alias nameit="nvim ~/.bash_aliases"
+alias tmuxit="nvim ~/.tmux.conf"
 
--- .config/nvim/lua/jkc/plugins/themes.lua
+++ .config/nvim/lua/jkc/plugins/themes.lua
@@ -1,21 +1,38 @@
 local config = {}
 
-local tokyonight = {
-  "folke/tokyonight.nvim",
-  config = function()
-    require("tokyonight").setup({
-        transparent = true,
-        styles = {
-            sidebars = "transparent",
-            floats = "transparent",
-        },
-    })
+local catpuccin = {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "macchiato",
+            transparent_background = true,
+        })
+        vim.cmd("colorscheme catppuccin")
+    end
+}
 
-    vim.cmd("colorscheme tokyonight")
-  end
+local lualine = {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "folke/noice.nvim",
+    },
+    config = function()
+        require('lualine').setup {
+            options = {
+                theme = 'catppuccin',
+                icons_enabled = true,
+                component_separators = '',
+                section_separators = '',
+            },
+        }
+    end
 }
 
 -- table.insert(config, onedark)
-table.insert(config, tokyonight)
+-- table.insert(config, tokyonight)
+table.insert(config, catpuccin)
+table.insert(config, lualine)
 
 return config
