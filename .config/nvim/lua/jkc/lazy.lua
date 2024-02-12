faf0dd91c24ac7ad3e9a122c0b9ebb51d6ab23ae
Jesus Capellan <jcapellan816@gmail.com>
2024-02-11 23:29:20 -0500

    Add formatting

--- .config/nvim/lua/jkc/lazy.lua
+++ .config/nvim/lua/jkc/lazy.lua
@@ -1,14 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("jkc.plugins", { change_detection = { notify = false } })
+require("lazy").setup({{import = "jkc.plugins"}, {import = "jkc.plugins.lsp"}}, {
+    install = {
+        colorscheme = { "catppuccin" },
+    },
+    change_detection = {
+        notify = false
+    },
+    checker = {
+        enable = true,
+        notify = false
+    },
+})
 
